5f8f71a38d98476c84c77b907af7c5fef4b1095b
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-02-04 20:17:37 -0300

    Adicioando novo controller na camada da web.ap

---
+++ src/Finance.Web.API/Controllers/IncomeSystemController.cs
@@ -0,0 +1,81 @@
+﻿using Finance.Domain.Entity.Entities.Systems.SystemExpenses;
+using Finance.Domain.Interfaces.Repositories.Systems.SystemExpenses;
+using Finance.Domain.Interfaces.Services.Systems.SystemExpenses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseSystemController : ControllerBase
+    {
+        private readonly IRepositorySystemExpense repositorySystemExpense;
+        private readonly IServiceSystemExpense serviceSystemExpense;
+        public ExpenseSystemController(
+            IRepositorySystemExpense repositorySystemExpense,
+            IServiceSystemExpense serviceSystemExpense
+            )
+        {
+            this.repositorySystemExpense = repositorySystemExpense;
+            this.serviceSystemExpense = serviceSystemExpense;
+        }
+
+
+        [Produces("application/json")]
+        [HttpPost("/api/AddSystemExpense")]
+        public async Task<object> AddSysteExpense(SystemExpense systemExpense)
+        {
+            await serviceSystemExpense.AddSystemExpense(systemExpense);
+            return systemExpense;
+        }
+
+        [Produces("application/json")]
+        [HttpPut("/api/UpdateSystemExpense")]
+        public async Task<object> UpdateSystemExpense(SystemExpense systemExpense)
+        {
+            await serviceSystemExpense.UpdateSystemExpense(systemExpense);
+            return Task.FromResult(systemExpense);
+        }
+
+        [Produces("application/json")]
+        [HttpDelete("/api/DeleteSystemExpense")]
+        public async Task<object> DeleteSystemExpense(int id)
+        {
+            try
+            {
+                var systemExpense = await repositorySystemExpense.GetEntityById(id);
+
+                await repositorySystemExpense.Delete(systemExpense);
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            return true;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("/api/SystemExpenseUserList")]
+        public async Task<object> SystemExpenseUserList(string emailUser)
+        {
+            return await repositorySystemExpense.SystemExpenseUserList(emailUser);
+        }
+
+        [Produces("application/json")]
+        [HttpGet("/api/GetExpenseSystem")]
+        public async Task<object> GetSystemExpense(int id)
+        {
+            return await repositorySystemExpense.GetEntityById(id);
+        }
+
+        [Produces("application/json")]
+        [HttpPost("/api/GenerateSystemExpenseCopy")]
+        public async Task<object> GenerateSystemExpenseCopy()
+        {
+            return await repositorySystemExpense.GenerateSystemExpenseCopy();
+        }
+
+    }
+}
